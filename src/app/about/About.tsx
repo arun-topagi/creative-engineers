1ed4d2e2d7391fd5979a183267eb8790ab0ca0c5
arpit2121 <arpitsingh9892@gmail.com>
2024-01-27 01:57:24 +0530

    airtable api integration is done

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -1,47 +1,93 @@
-"use client"
+"use client";
 
-import TextField from '@/components/input';
-import { useScreenDetector } from '@/hooks/useScreenDetector';
-import { Typography } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
-import React from 'react'
+import TextField from "@/components/input";
+import { useScreenDetector } from "@/hooks/useScreenDetector";
+import { Typography } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+import React, { useState } from "react";
 
 function About() {
-    const {isMobile} = useScreenDetector();
-    const onFinish = (values: any) => {
-        console.log('Success:', values);
-      };
-      
-      const onFinishFailed = (errorInfo: any) => {
-        console.log('Failed:', errorInfo);
-      };
-      
-      type FieldType = {
-        username?: string;
-        password?: string;
-        remember?: string;
-      };
+  const { isMobile } = useScreenDetector();
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const [showThankYou, setShowThankYou] = useState(false);
+
+  type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+  };
   return (
-    <div style={{marginLeft: isMobile ? '15px' : "30px" , marginRight: isMobile ? '15px' : '30px'}}>
-        <div style={{textAlign:'center', marginTop:'60px', marginBottom: isMobile ? '50px' : '80px'}}>
-            <Typography style={{marginTop:'10px', fontSize:'1.875rem', fontWeight: 400, lineHeight:'62px', fontFamily:'Montserrat'}}>Get in touch</Typography>
+    <div
+      style={{
+        marginLeft: isMobile ? "15px" : "30px",
+        marginRight: isMobile ? "15px" : "30px",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "60px",
+          marginBottom: isMobile ? "50px" : "80px",
+        }}
+      >
+        <Typography
+          style={{
+            marginTop: "10px",
+            fontSize: "1.875rem",
+            fontWeight: 400,
+            lineHeight: "62px",
+            fontFamily: "Montserrat",
+          }}
+        >
+          Get in touch
+        </Typography>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "1140px",
+          marginLeft: "auto",
+          marginRight: "auto",
+          textAlign: isMobile ? "center" : "initial",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "stretch",
+            marginBottom: "75px",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: isMobile ? "90%" : "70%",
+            maxWidth: isMobile ? "470px" : "",
+          }}
+        >
+          <div>
+            {showThankYou ? (
+              <div
+                style={{ marginTop: "1rem", fontSize: "36px", color: "green", textAlign:'center' }}
+              >
+                Thank You! Your message has been submitted.
+              </div>
+            ) : (
+              <TextField setShowThankYou={setShowThankYou} />
+            )}
+          </div>
         </div>
-        <div style={{width:'100%', maxWidth:'1140px', marginLeft:'auto', marginRight:'auto', textAlign: isMobile ? 'center': 'initial'}}>
-            <div style={{display:'flex', flexDirection:'column', alignItems:'stretch', marginBottom:'75px', marginLeft:'auto', marginRight: 'auto', width: isMobile ? '90%' : '70%', maxWidth: isMobile ? '470px' : ''}}>
-                <div>
-                    <TextField/>
-                </div>
-                {/* <div> on success</div>
-                <div> on errror </div> */}
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
-
-
+export default About;
 
 /*
     there will be frist div will show normally 
